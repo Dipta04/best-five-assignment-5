3787049ec04ac0a62ca2e223b51239aedb827d4c
Dipta04 <diptakarmakar38@gmail.com>
2022-08-22 12:24:38 +0600

    new commit

---
+++ js/player.js
@@ -0,0 +1,91 @@
+document.getElementById('btn-neymar').addEventListener('click',function()
+{
+    const liFirst=document.getElementById('li-first');   
+    const liSecond=document.getElementById('li-second');
+    const liThird=document.getElementById('li-third');
+    const liFourth=document.getElementById('li-forth');
+    const liFifth=document.getElementById('li-fifth');
+
+liFirst.innerText="1.Neymar Jr";
+liSecond.innerText="2.Lionel Messi";
+liThird.innerText="3.Kylian Mbappé";
+liFourth.innerText="4.Vitor Machado";
+liFifth.innerText="5.Sergio Ramos";
+})
+// messi
+document.getElementById('btn-messi').addEventListener('click',function()
+{
+    const liFirst=document.getElementById('li-first');   
+    const liSecond=document.getElementById('li-second');
+    const liThird=document.getElementById('li-third');
+    const liFourth=document.getElementById('li-forth');
+    const liFifth=document.getElementById('li-fifth');
+
+liFirst.innerText="1.Lionel Messi";
+liSecond.innerText="2.Neymar Jr";
+liThird.innerText="3.Kylian Mbappé";
+liFourth.innerText="4.Vitor Machado";
+liFifth.innerText="5.Sergio Ramos";
+})
+// mbappe
+document.getElementById('btn-mbappe').addEventListener('click',function()
+{
+    const liFirst=document.getElementById('li-first');   
+    const liSecond=document.getElementById('li-second');
+    const liThird=document.getElementById('li-third');
+    const liFourth=document.getElementById('li-forth');
+    const liFifth=document.getElementById('li-fifth');
+
+liFirst.innerText="1.Kylian Mbappé";
+liSecond.innerText="2.Lionel Messi";
+liThird.innerText="3.Neymar Jr";
+liFourth.innerText="4.Vitor Machado";
+liFifth.innerText="5.Sergio Ramos";
+})
+// Vitor
+document.getElementById('btn-vitor').addEventListener('click',function()
+{
+    const liFirst=document.getElementById('li-first');   
+    const liSecond=document.getElementById('li-second');
+    const liThird=document.getElementById('li-third');
+    const liFourth=document.getElementById('li-forth');  
+    const liFifth=document.getElementById('li-fifth');
+
+liFirst.innerText="1.Vitor Machado";
+liSecond.innerText="2.Lionel Messi";
+liThird.innerText="3.Neymar Jr";
+liFourth.innerText="4.Kylian Mbappé";
+liFifth.innerText="5.Sergio Ramos";
+})
+// ramos
+document.getElementById('btn-ramos').addEventListener('click',function()
+{
+    const liFirst=document.getElementById('li-first');   
+    const liSecond=document.getElementById('li-second');
+    const liThird=document.getElementById('li-third');
+    const liFourth=document.getElementById('li-forth');
+    const liFifth=document.getElementById('li-fifth');
+
+liFirst.innerText="1.Sergio Ramos";
+liSecond.innerText="2.Lionel Messi";
+liThird.innerText="3.Neymar Jr";
+liFourth.innerText="4.Kylian Mbappé";
+liFifth.innerText="5.Vitor Machado";
+})
+// Renato
+document.getElementById('btn-renato').addEventListener('click',function()
+{
+    const liFirst=document.getElementById('li-first');   
+    const liSecond=document.getElementById('li-second');
+    const liThird=document.getElementById('li-third');
+    const liFourth=document.getElementById('li-forth');
+    const liFifth=document.getElementById('li-fifth');
+
+liFirst.innerText="1.Renato Sanches";
+liSecond.innerText="2.Lionel Messi";
+liThird.innerText="3.Neymar Jr";
+liFourth.innerText="4.Kylian Mbappé";
+liFifth.innerText="5.Vitor Machado";
+})
+
+
